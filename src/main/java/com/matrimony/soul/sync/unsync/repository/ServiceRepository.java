671345a672ca1c99098ebc904cc5007dce9076a2
Ayan Halder <114690741+ayanhalder27@users.noreply.github.com>
2026-09-22 23:27:34 +0600

    ServiceRepository issue solved

--- src/main/java/com/matrimony/soul/sync/unsync/repository/ServiceRepository.java
+++ src/main/java/com/matrimony/soul/sync/unsync/repository/ServiceRepository.java
@@ -3,7 +3,7 @@ package com.matrimony.soul.sync.unsync.repository;
 import com.matrimony.soul.sync.unsync.repository.mapper.ServiceMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
+import com.matrimony.soul.sync.unsync.domain.Service;
 
 @Repository
 public class ServiceRepository {
@@ -14,12 +14,14 @@ public class ServiceRepository {
     }
 
     public Service get(int id){
-        return (Service) jdbcTemplate.queryForObject("SELECT * FROM Services WHERE id=?",new ServiceMapper(),id);
+        return jdbcTemplate.queryForObject("SELECT * FROM Services WHERE id=?",new ServiceMapper(),id);
     }
 
     public int insert(Service service){
-        return jdbcTemplate.update("INSERT INTO Services(service_name,description,price,organizer_id) VALUES(?, ?, ?,  ?);");
-
-
+        return jdbcTemplate.update("INSERT INTO Services(service_name,description,price,organizer_id) VALUES(?, ?, ?,  ?);",
+                service.getService_name(),
+                service.getDescription(),
+                service.getPrice(),
+                service.getOrganizer_id());
     }
 }
